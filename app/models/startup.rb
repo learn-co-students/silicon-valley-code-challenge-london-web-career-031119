694a69a6ece82eecfc73001b611b33d597d55488
deeseert <gio.amms@gmail.com>
2019-03-19 21:15:11 +0000

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,53 @@
 class Startup
+  attr_accessor :name
+  attr_reader :founder, :domain
+  @@all = []
 
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pivot(name, domain)
+    @name = name
+    @domain = domain
+  end
+
+  def self.find_by_founder(founder_name)
+    Startup.all.find {|i| i.founder == founder_name}
+  end
+
+  def self.domains
+    Startup.all.map {|i| i.domain}.uniq
+    Startup.all.map(&:domain).uniq
+    # should return an array of all of the different startup domains
+  end
+
+  def sign_contract(venture_capitalist, type, investment)
+    FundingRound.new(self, venture_capitalist, type, investment)
+  end
+
+  def fundings
+    FundingRound.all.select {|i| i.startup == self}
+  end
+
+  def num_funding_rounds
+    fundings.map { |i| i.investment}.reduce(:+)
+  end
+
+  def investors
+    fundings.map {|i| i.venture_capitalist.name}.uniq
+  end
+
+  def big_investors
+    var1 = fundings.map {|i| i.venture_capitalist}
+    var2 = VentureCapitalist.all.select {|i| i.total_worth > 1000000000}
+    (var1 & var2).uniq
+  end
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,48 @@
 class VentureCapitalist
+  attr_accessor :name, :total_worth
+  @@all = []
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    VentureCapitalist.all.select {|i| i.total_worth > 1000000000}
+    #returns an array of all venture capitalists in the Trés Commas club
+    #(they have more than 1,000,000,000 total_worth)
+  end
+
+  def offer_contract(startup, type, investment)
+    # binding.pry
+    FundingRound.new(startup, self, type, investment)
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|i|i.venture_capitalist == self }
+  end
+
+  def portfolio
+    funding_rounds.map {|i|i.startup}.uniq
+  end
+
+  def biggest_investment
+    funding_rounds.max_by {|i| i.investment}
+  end
+
+  def invested(domain)
+    counter = 0
+    funding_rounds.each do |x|
+      if x.startup.domain == domain
+        counter =+ x.investment
+      end
+    end
+    counter
+  end
 
 end
