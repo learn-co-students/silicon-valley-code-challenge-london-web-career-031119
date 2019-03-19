e50d859807c175680be548a63d63db6b415c235a
mrsalvini <dsgmcosta@gmail.com>
2019-03-19 15:03:25 +0000

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,37 @@
 class FundingRound
+    attr_reader :startup, :venture_capitalist, :type, :investment
+
+    @@all = []
+
+    def initialize(startup, venture_capitalist, type, investment)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type
+        @investment = investment
+        FundingRound.all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
+
+# FundingRound#startup
+# returns the startup object for that given funding round
+# Once a funding round is created, I should not be able to change the startup
+
+# FundingRound#venture_capitalist
+# returns the venture capitalist object for that given funding round
+# Once a funding round is created, I should not be able to change the venture capitalist
+
+# FundingRound#type
+# returns a string that is the type of funding round
+# Examples include: Angel, Pre-Seed, Seed, Series A, Series B, Series C, etc.
+
+# FundingRound#investment
+# returns a number that is the amount invested during this funding round
+# This should be a float that is not a negative number.
+
+# FundingRound.all
+# returns all of the funding rounds
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,24 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+s1 = Startup.new("Startup 1", "Founder 1", "Domain 1")
+s2 = Startup.new("Startup 2", "Founder 2", "Domain 2")
+s3 = Startup.new("Startup 3", "Founder 3", "Domain 3")
+
+vc1 = VentureCapitalist.new("VC1", 20000000)
+vc2 = VentureCapitalist.new("VC2", 5000000000)
+vc3 = VentureCapitalist.new("VC3", 6000000000)
+
+fr1 = FundingRound.new(s1, vc1, "Angel", 500000)
+fr2 = FundingRound.new(s1, vc2, "Angel", 1000000)
+# fr3 = FundingRound.new(s1, vc3, "Angel", 3000000)
+fr4 = FundingRound.new(s2, vc1, "Seed", 1600000)
+fr5 = FundingRound.new(s2, vc2, "Seed", 1800000)
+fr6 = FundingRound.new(s3, vc3, "Angel", 3000000)
+fr7 = FundingRound.new(s1, vc2, "Wack", 3000000)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
+
+# s4 = Startup.new("Startup 4", "Founder 4", "Domain 1")
+# fr7 = FundingRound.new(s1, vc2, "Wack", 3000000)
