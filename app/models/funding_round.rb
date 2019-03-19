47e891688d6e097121148fd3676e22cbf83eff7d
“Ranjit <ranjit.saimbi@gmail.com>
2019-03-19 16:27:34 +0000

    Done

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,18 @@
 class FundingRound
 
+  attr_reader :startup, :venture_capitalist, :type, :investment
+  @@all = []
+
+  def initialize(startup, venture_capitalist, type, investment)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    @investment = investment > 0 ? investment.round : 0
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,40 @@
 class VentureCapitalist
+  attr_accessor :name, :total_worth
+  @@all = []
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    @@all.select {|vc| vc.total_worth >= 1_000_000_000}
+  end
+
+  def offer_contract(startup, type, investment)
+    FundingRound.new(startup, self, type, investment)
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|funding_round| funding_round.venture_capitalist == self}
+  end
+
+  def portfolio
+    self.funding_rounds.map {|funding_round|funding_round.startup.name}.uniq
+  end
+
+  def biggest_investment
+    self.funding_rounds.max_by {|funding_round|funding_round.investment}
+  end
+
+  def invested(domain)
+    array_of_domains = self.funding_rounds.select {|funding_round|funding_round.startup.domain == domain}
+    array_of_domains.map {|funding_round| funding_round.investment}.reduce(:+)
+  end
 
 end
