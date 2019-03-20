262231a50d11240b113876ad184136dc976292da
flatiron <flatiron@BERLIN.local>
2019-03-20 11:04:54 +0000

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,39 @@
+require_relative './venture_capitalist.rb'
+require_relative './startup.rb'
+
 class FundingRound
+  attr_reader :startup, :vent_capitalist, :ammount_invested, :type_investment
+
+  @@all = []
+
+  def initialize(startup, vent_capitalist, type_investment = nil, ammount_invested)
+    @startup = startup
+    @vent_capitalist = vent_capitalist
+    @ammount_invested = ammount_invested
+    @type_investment = type_investment
+    @@all << self
+  end
+
+  def startup
+    @startup
+  end
+
+  def venture_capitalist
+    @vent_capitalist
+  end
+
+  def type
+    @type_investment
+  end
+
+  def investment
+   temp_investment = FundingRound.all.map {|i| i.ammount_invested}
+   temp_investment.inject(0){|sum,x| sum + x }
+
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,69 @@
-class VentureCapitalist
 
+class VentureCapitalist
+  attr_accessor :name, :tot_worth, :tcc
+
+  @@all = []
+
+  def initialize (name, tot_worth, tcc = nil)
+    @name = name
+    @tot_worth = tot_worth
+    @@all << self
+    @tcc = tcc
+    if tot_worth > 1000000000
+    @tcc = true
+  end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    @@all.select {|i| i.tot_worth > 1000000000}
+  end
+
+  def total_worth
+  temp_worth = VentureCapitalist.all.find {|i| i.tot_worth}
+  end
+
+  def offer_contract (startup, type, ammount)
+    temp_offer = Startup.all.find {|i| i == startup}
+    if temp_offer
+      FundingRound.new(startup, self, type, ammount)
+    else
+      return "Incorrect Information Given"
+    end
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|i| i.vent_capitalist == self}
+  end
+
+  def portfolio
+    temp_startup = FundingRound.all.select {|i| i.vent_capitalist == self}
+    temp_startup.map {|i| i.startup.name}.uniq
+  end
+
+  def biggest_investment
+    temp_startup = FundingRound.all.select {|i| i.vent_capitalist == self}
+    temp_startup.map {|i| i.ammount_invested}.max
+
+  end
+
+  def invested(domain_check)
+  temp_startup = FundingRound.all.select {|i| i.vent_capitalist == self}
+  temp_final = temp_startup.select {|i| i.startup.domain == domain_check}
+  temp_count = temp_final.map{|i| i.ammount_invested}
+  temp_count.inject(0){|sum,x| sum + x }
+  end
+    public :offer_contract
 end
+
+  # VentureCapitalist funding_rounds
+  # returns an array of all funding rounds for that venture capitalist
+  # # VentureCapitalist#portfolio
+  # Returns a unique list of all startups this venture capitalist has funded
+  # VentureCapitalist#biggest_investment
+  # returns the largest funding round given by this venture capitalist
+  # VentureCapitalist#invested
+  # given a domain string, returns the total amount invested in that domain
